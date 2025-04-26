825dc2c6907327efca018de6c0164eaec23920b7
radhikari-3 <rohit.adhikari@outlook.com>
2025-04-26 12:04:20 +0100

    added test folder

---
+++ tests/test_basic.py
@@ -0,0 +1,16 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_homepage(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_dashboard(client):
+    response = client.get('/login')
+    assert response.status_code == 200
