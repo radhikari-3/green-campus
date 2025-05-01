ec5e44df9d806fa5a3a527307bcad6e2f54615a0
radhikari-3 <rohit.adhikari@outlook.com>
2025-05-01 18:41:10 +0100

    update

--- config.py
+++ config.py
@@ -7,16 +7,8 @@ load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or b'WR#&f&+%78er0we=%799eww+#7^90-;s'
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'your_email@gmail.com'  
-    MAIL_PASSWORD = 'your_app_password'
     UPLOAD_FOLDER = os.path.join(basedir, 'app', 'data', 'uploads')
     MAX_CONTENT_LENGTH = 1 * 1024 * 1024
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
-
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('DB_USERNAME')}:{os.environ.get('DB_PASSWORD')}@localhost/{os.environ.get('DB_NAME')}"
-
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('DB_USERNAME', 'default_user')}:{os.environ.get('DB_PASSWORD', 'default_password')}@localhost/{os.environ.get('DB_NAME', 'default_db')}"
