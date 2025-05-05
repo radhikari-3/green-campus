0b2c7181a38e301b421e56c16800732ce22e57f7
Melissa Zulle <melissazulle@Melissas-MacBook-Air.local>
2025-05-05 15:10:10 +0100

    Fixed edit issues and views code refactored

--- app/forms.py
+++ app/forms.py
@@ -8,6 +8,11 @@ from wtforms.fields.numeric import IntegerField, FloatField
 from wtforms.validators import DataRequired, NumberRange
 
 
+def validate_expiry_date(self, field):
+    today = datetime.date.today()   # get today's date (no time part)
+    if field.data < today:          # field.data is already a date (because you're using DateField)
+        raise ValidationError("You can't upload expired goods!")
+
 class ChooseForm(FlaskForm):
     choice = HiddenField('Choice')
 
@@ -17,24 +22,24 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-class InventoryForm(FlaskForm):
-    edit = HiddenField ('Edit',default = "-1")
+class AddProductForm(FlaskForm):
+    # edit = HiddenField ('Edit',default = "-1")
     name = StringField('Product Name', validators=[DataRequired()])
-    expiry_date = DateField('Expiry Date', validators=[DataRequired()])
+    expiry_date = DateField('Expiry Date', validators=[DataRequired(), validate_expiry_date])
     units = IntegerField('Units', validators= [NumberRange(1,30, "You can only upload from 1 to 30 units.")])
     price = FloatField('Price', validators= [DataRequired()])
     location = StringField('Location', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-    def validate_expiry_date(self, field):
-        today = datetime.date.today()  # get today's date (no time part)
-        expiry_date = field.data       # field.data is already a date (because you're using DateField)
-
-        if expiry_date < today:
-            raise ValidationError("You can't upload expired goods!")
-
 class DeleteForm(FlaskForm):
     delete_product = HiddenField('delete_product')
 
 class EditProductForm(FlaskForm):
-    edit_product = HiddenField('edit_product')
+    product_id = HiddenField('Product ID', default="-1")
+    name = StringField('Product Name', validators=[DataRequired()])
+    expiry_date = DateField('Expiry Date', validators=[DataRequired(), validate_expiry_date])
+    units = IntegerField('Units', validators=[NumberRange(1, 30, "You can only upload from 1 to 30 units.")])
+    price = FloatField('Price', validators=[DataRequired()])
+    location = StringField('Location', validators=[DataRequired()])
+    submit = SubmitField('Update')
+
